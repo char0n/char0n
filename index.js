b608a01cec39925d3ae08ba838b331377a180857
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-08-23 14:41:19 +0200

    fix: align github stats

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ md.use(emoji);
   [:arrow_right: More tweets](${twitterUrl})
   \n\n
   ## GitHub Stats\n
-  ![GitHub Stats](https://github-readme-stats.vercel.app/api?username=char0n&show_icons=true&hide_border=true)
+  ![GitHub Stats](https://github-readme-stats.vercel.app/api?username=char0n&show_icons=true&hide_border=true&count_private=true&include_all_commits=true =x180em)
   ![GitHub Most Used Languages](https://github-readme-stats.vercel.app/api/top-langs/?username=char0n&show_icons=true&hide_border=true&layout=compact&langs_count=8)`;
 
     const result = md.render(text);
